721e111bb6a45ca54f77133bca65562d73f4b309
OgtayCodeAcademy <tu89fpk8j@code.edu.az>
2023-11-27 14:24:10 +0400

    User Admin Card Request (Confirm/decline)

---
+++ React Practice/admin-confirm-request/src/Companents/AdminPanel/index.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+
+export default function AdminPanel({ CurrentUser, users }) {
+  let filtred_users = users.filter((user) => user.cards.length > 0);
+  let filtred_cards;
+  let cards = [];
+  let cards_array = [];
+  let [cardsOwner, setCardsOwner] = useState({})
+  cards.push(filtred_users.map((user) => user.cards.map((card) => card)));
+  cards[0].map((card) => {
+    cards_array = [...card];
+  });
+  return (
+    <>
+      <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '20px', marginTop: '100px'}} className="container">
+        {cards_array.map((CARD, idx) => {
+          return (
+            <Card key={idx} sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image={CARD.image}
+                title="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {CARD.tittle}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {CARD.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button variant="contained" style={{color: 'white', backgroundColor: 'seagreen'}} size="small" onClick={()=>{
+                  axios.get(`https://6564178bceac41c0761d637a.mockapi.io/users/${CARD.id}`)
+                  .then((response)=>{
+                    setCardsOwner(response.data)
+                  })
+                  console.log(cardsOwner.cards);
+                  filtred_cards = cardsOwner.cards.filter((card)=> card.tittle !== CARD.tittle && card.description !== CARD.description)
+                  setCardsOwner({...cardsOwner, cards: [...filtred_cards, {description: CARD.description, id: CARD.id, image: CARD.image, tittle: CARD.tittle, isConfirmed: true}]})
+                  axios.put(`https://6564178bceac41c0761d637a.mockapi.io/users`, cardsOwner)
+                }}>Confirm</Button>
+                <Button variant="contained" style={{color: 'white', backgroundColor: 'red'}} size="small">Decline</Button>
+              </CardActions>
+            </Card>
+          );
+        })}
+      </div>
+    </>
+  );
+}
---
+++ React Practice/admin-confirm-request/src/Companents/Cards/index.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useReducer, useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+
+function reducer(state, action){
+  const type = action.type
+  switch (type) {
+    case 'addNewCard':
+      let current_user = JSON.parse(localStorage.getItem('currentUser'))
+      current_user = {...current_user, cards: [...current_user.cards, {
+        id: action.userID,
+        image: action.cardImage,
+        description: action.cardDescription,
+        tittle: action.cardTittle,
+        isConfirmed: false
+      }]};
+      axios.put(`https://6564178bceac41c0761d637a.mockapi.io/users/${action.userID}`, current_user)
+      localStorage.setItem('currentUser', JSON.stringify(current_user))
+    break;
+    default:
+      return state
+  }
+}
+
+export default function CardsSection({CurrentUser}) {
+  let [newCardInputs, setNewCardInputs] = useState({image: "https://www.birdlife.org/wp-content/uploads/2021/06/Hummingbird-Norbert-Hentges-Unsplash-edited-scaled.jpg", tittle: 'HummingBird', description: 'Hummingbirds have compact, strongly muscled bodies and rather long, bladelike wings that, unlike the wings of other birds, articulate (connect) to the body only from the shoulder joint. '})
+  let [currentUser, dispatch] = useReducer(reducer, CurrentUser)
+  return (
+    <>
+      <div style={{display: 'flex', justifyContent: 'center', paddingTop: '100px'}} className="container">
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={newCardInputs.image}
+            title= {newCardInputs.tittle}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {newCardInputs.tittle}
+            </Typography>
+            <input type="text" placeholder="Tittle" onChange={(e)=>{
+              setNewCardInputs({...newCardInputs, tittle: e.target.value});
+            }}/>
+            <Typography variant="body2" color="text.secondary">
+              {newCardInputs.description}
+            </Typography>
+            <input type="text" placeholder="Description" onChange={(e)=>{
+              setNewCardInputs({...newCardInputs, description: e.target.value});
+            }}/>
+          </CardContent>
+          <CardActions>
+            <Button size="small" onClick={()=>{
+              dispatch({type: 'addNewCard', 
+              user: CurrentUser, 
+              userID: CurrentUser.id,
+              cardTittle: newCardInputs.tittle, 
+              cardDescription: newCardInputs.description,
+              cardImage: newCardInputs.image,
+            })
+            }}>Send to approval</Button>
+            <input type="text" placeholder="Image Link" onChange={(e)=>{
+              setNewCardInputs({...newCardInputs, image: e.target.value});
+            }}/>
+          </CardActions>
+        </Card>
+      </div>
+    </>
+  );
+}
